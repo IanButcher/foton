42ca681939d0a3fe9c9bcd8cbf0a40426968d312
IanButcher <115805201+IanButcher@users.noreply.github.com>
2024-08-26 12:55:05 -0300

    Archivos Obtener IP

---
+++ fotonsito2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+
+namespace MyHttpServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<string> ipAddresses = new List<string>();
+        private static List<string> temperatures = new List<string>();
+
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString();
+
+            if (!string.IsNullOrEmpty(clientIp))
+            {
+                // Guardar la IP en el array
+                ipAddresses.Add(clientIp);
+            }
+            // redirigir
+            return RedirectToAction("GetIps");
+        }
+
+        [HttpGet("/ips")]
+        public IActionResult GetIps()
+        {
+            // Devuelve todas las IPs almacenadas
+            return Json(ipAddresses);
+        }
+
+        [HttpPost("/temperature")]
+        public IActionResult PostTemperature(string temperature)
+        {
+            if (!string.IsNullOrEmpty(temperature))
+            {
+                // Guardar la temperatura en el array
+                temperatures.Add(temperature);
+            }
+
+            // Redirigir o mostrar un mensaje
+            return RedirectToAction("Index");
+        }
+    }
+}
